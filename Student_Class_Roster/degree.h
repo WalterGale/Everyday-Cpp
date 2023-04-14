f39ca67f24eef0f596b702ed4a09d055b8a7e85b
Walter R Galeziowski <129920862+WalterGale@users.noreply.github.com>
2023-04-13 20:30:51 -0400

    Add files via upload

---
+++ Student_Class_Roster/degree.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef DEGREE_H
+#define DEGREE_H
+
+#include <iostream>
+using namespace std;
+
+// Part C: Define an enumerated data type "Degree Program" for the degree programs containing the data type values: SECURITY, NETWORK and SOFTWARE.
+// This information should be included in the degree.h file.
+
+enum class DegreeProgram
+{
+	SECURITY, NETWORK, SOFTWARE
+};
+
+#endif
